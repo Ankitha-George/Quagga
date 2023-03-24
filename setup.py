94fabdb900ff79c7171d24ade12b3b5208cba1ac
Ankitha-George <93380769+Ankitha-George@users.noreply.github.com>
2023-03-24 14:43:09 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='Quagga',
+    version='0.1',
+    author='Ankitha',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'python-crfsuite'
+    ],
+)
